4de1070584a70ebaa79efff3d8712d465574b859
Krka <nikolakrkickrka@gmail.com>
2023-08-13 14:34:04 +0200

    Showed areas on my start screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import './App.css'
 import axios from 'axios'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [areas, setAreas] = useState([])
   useEffect(() => {
     axios.get('http://localhost:3001/areas').then((response) => {
-      console.log(response.data)
+      setAreas(response.data)
     })
   }, [])
   return (
     <div className="App">
+      {areas.map((area, key) => {
+        return (
+          <div key={key}>
+            <h1>{area.area_name}</h1>
+          </div>
+        )
+      })}
     </div>
   );
 }
