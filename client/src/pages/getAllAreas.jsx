47a58edb549950d6727b5a18632a80bd69c9629d
Krka <nikolakrkickrka@gmail.com>
2023-09-13 20:04:19 +0200

    Added confirm dialog for deleting area

--- client/src/pages/getAllAreas.jsx
+++ client/src/pages/getAllAreas.jsx
@@ -1,6 +1,6 @@
 // libraries
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 
 // queries
@@ -13,6 +13,8 @@ import './css/getAllAreas.css'
 import { AiFillEdit } from 'react-icons/ai'
 import { RiDeleteBin6Fill } from 'react-icons/ri'
 import { FaQuestion } from 'react-icons/fa'
+import { BsFillCheckCircleFill } from 'react-icons/bs'
+import { RxCrossCircled } from 'react-icons/rx'
 
 // components
 import { AddArea } from '../components/addArea'
@@ -21,8 +23,7 @@ import { AddArea } from '../components/addArea'
 import { deleteAreaMutation } from './queries/areaQueries'
 
 export const GetAllAreas = () => {
-
-  const navigate = useNavigate()
+  const [deleteButtonId, setDeleteButtonId] = useState(0)
 
   const queryClient = useQueryClient()
 
@@ -39,25 +40,53 @@ export const GetAllAreas = () => {
           <div className='row'>
             {areas.map((area, key) => {
               return (
-                <div className='col-sm-12 col-md-6 col-lg-4'>
-                  <div className='area' key={key}>
-                    <h1 className='areaName'>{area.area_name}</h1>
-                    <div className='areaButtons'>
-                      <button className='areaButton'>
-                        <AiFillEdit />
-                      </button>
-                      <button 
-                        onClick={ async () =>  await deleteArea(area.id)}
-                        className='areaButton'>
-                        <RiDeleteBin6Fill />
-                      </button>
-                      <button
-                        onClick={() => navigate(`/areaDetails/${area.id}`)}
-                        className='areaButton'
-                      >
-                        <FaQuestion />
-                      </button>
-                    </div>
+                <div className='col-sm-12 col-md-6 col-lg-4' key={key}>
+                  <div>
+                    {deleteButtonId !== area.id ? (
+                      <div className='area'>
+                        <h1 className='areaName'>{area.area_name}</h1>
+                        <div className='areaButtons'>
+                          <button className='areaButton'>
+                            <AiFillEdit />
+                          </button>
+                          <button
+                            onClick={() => setDeleteButtonId(area.id)}
+                            //onClick={ async () =>  await deleteArea(area.id)}
+                            className='areaButton'
+                          >
+                            <RiDeleteBin6Fill />
+                          </button>
+                          <Link
+                            to={`/areaDetails/${area.id}`}
+                            className='areaButton'
+                          >
+                            <FaQuestion />
+                          </Link>
+                        </div>
+                      </div>
+                    ) : (
+                      <div className='confirmDeleteArea'>
+                        <h1 className='areaName'>Да ли сте сигурни?</h1>
+                        <h6 className='areaMessage'>
+                          Брисањем ове области обрисаћете сва питања везана за
+                          ову област и све њихове одговаре.
+                        </h6>
+                        <div className='confirmButtons'>
+                          <button
+                            onClick={ async () =>  await deleteArea(area.id)}
+                            className='areaButton'
+                          >
+                            <BsFillCheckCircleFill />
+                          </button>
+                          <button
+                            onClick={() => setDeleteButtonId(0)}
+                            className='areaButton'
+                          >
+                            <RxCrossCircled />
+                          </button>
+                        </div>
+                      </div>
+                    )}
                   </div>
                 </div>
               )
