6a98bf529e46a820115d98cd9e4d6c24a5ee609d
Krka <nikolakrkickrka@gmail.com>
2023-09-21 19:37:24 +0200

    Started getAllUsers page

--- client/src/pages/getAllUsers.jsx
+++ client/src/pages/getAllUsers.jsx
@@ -6,43 +6,64 @@ import { useQuery } from '@tanstack/react-query'
 // queries
 import { getAllNonadminUsersQuery } from '../queries/userQueries'
 
+// css
+import '../css/getAllUsers.css'
+
 export const GetAllUsers = () => {
 
   const { data: users } = useQuery(getAllNonadminUsersQuery())
 
-  const tableData = useMemo(() => users, [])
-  const tableColumns = useMemo(() => [
-    {
-        Header: "Идентификатор",
-        accessor: "id"
-    },
-    {
-        Header: "Име",
-        accessor: "first_name"
-    },
-    {
-        Header: "Презиме",
-        accessor: "last_name"
-    },
-    {
-        Header: "Надимак",
-        accessor: "nickname"
-    },
-    {
-        Header: "ГСС код - шифрован",
-        accessor: "GSS_identification"
-    },
-    {
-        Header: "Тренутно ради тест",
-        accessor: "logged_in_for_testing"
-    },
-    {
-        Header: "Администратор",
-        accessor: "admin"
-    }
-  ], [])
+  const tableData = useMemo(() => users, [users])
+  const tableColumns = useMemo(
+    () => [
+      {
+        Header: 'ГСС број',
+        accessor: 'GSS_identification'
+      },
+      {
+        Header: 'Име',
+        accessor: 'first_name'
+      },
+      {
+        Header: 'Презиме',
+        accessor: 'last_name'
+      },
+      {
+        Header: 'Надимак',
+        accessor: 'nickname'
+      }
+    ],
+    []
+  )
 
-  const table = useTable({columns: tableColumns, data: tableData})
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns: tableColumns, data: tableData })
 
-  return (<div></div>)
+  return (
+    <div className='centered'>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map(row => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => (
+                  <td {...cell.getCellProps()}> {cell.render('Cell')} </td>
+                ))}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
 }
